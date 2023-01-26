3764b74cdace0f41c35fe61f97b7dccb27736e16
franciscoassumpcao <38674113+franciscoassumpcao@users.noreply.github.com>
2023-01-26 11:22:20 +0100

    Pessoa Mostrando

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ let persons = []
 const endpointDaAPI = 'https://dsortapi-apim.azure-api.net/api/Person/getAllPerson';
 
 getBuscarTodosDocumentosDaAPI();
-
+const listaPessoasDiv = document.getElementById('listaPessoas');
 
 async function getBuscarTodosDocumentosDaAPI(){
     const conexaoAllPerson = await fetch(endpointDaAPI)
     persons = await conexaoAllPerson.json();
     console.log(persons);
     
+    persons.forEach(pessoa => {
+        listaPessoasDiv.innerHTML += `<h2>ID: ${pessoa.id}, Name: ${pessoa.Name}</h2>`;
+    });
     
 
    
